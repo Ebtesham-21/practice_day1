a17a04661623ba40400cd8e73378b0bd06b4d3f7
Ebtesham Hossain <ebteshamhossain98@gmail.com>
2023-04-06 13:39:48 +0000

    abc

--- .ccls-cache/@home@runner@practiceday1/main.c
+++ .ccls-cache/@home@runner@practiceday1/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+int text()
+{
+  printf("Recently I heard that you’ve achieved 95% marks in your exam.\n");
+  printf("This is brilliant! \n");
+  printf
+  
+}
+
 int main(void) {
   printf("Hello World\n");
   return 0;
